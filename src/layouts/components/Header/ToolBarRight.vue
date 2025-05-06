116cf0ccfa53f47ce069f28e8e7babaf6d789bdc
ge <528636582@qq.com>
2025-05-06 13:16:40 +0800

    feat: 更新用户登录逻辑

--- src/layouts/components/Header/ToolBarRight.vue
+++ src/layouts/components/Header/ToolBarRight.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="tool-bar-ri">
     <div class="header-icon">
-      <AssemblySize id="assemblySize" />
+      <!-- <AssemblySize id="assemblySize" />
       <Language id="language" />
       <SearchMenu id="searchMenu" />
       <ThemeSetting id="themeSetting" />
       <Message id="message" />
-      <Fullscreen id="fullscreen" />
+      <Fullscreen id="fullscreen" /> -->
     </div>
     <span class="username">{{ username }}</span>
     <Avatar />
@@ -16,12 +16,12 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useUserStore } from "@/stores/modules/user";
-import AssemblySize from "./components/AssemblySize.vue";
-import Language from "./components/Language.vue";
-import SearchMenu from "./components/SearchMenu.vue";
-import ThemeSetting from "./components/ThemeSetting.vue";
-import Message from "./components/Message.vue";
-import Fullscreen from "./components/Fullscreen.vue";
+// import AssemblySize from "./components/AssemblySize.vue";
+// import Language from "./components/Language.vue";
+// import SearchMenu from "./components/SearchMenu.vue";
+// import ThemeSetting from "./components/ThemeSetting.vue";
+// import Message from "./components/Message.vue";
+// import Fullscreen from "./components/Fullscreen.vue";
 import Avatar from "./components/Avatar.vue";
 
 const userStore = useUserStore();
--- src/views/Login/components/LoginForm.vue
+++ src/views/Login/components/LoginForm.vue
@@ -33,7 +33,7 @@ import { useRouter } from "vue-router";
 import { HOME_URL } from "@/config";
 // import { getTimeState } from "@/utils";
 import { Login } from "@/api/interface";
-import { ElNotification } from "element-plus";
+// import { ElNotification } from "element-plus";
 import { loginApi } from "@/api/modules/login";
 import { useUserStore } from "@/stores/modules/user";
 import { useTabsStore } from "@/stores/modules/tabs";
@@ -87,13 +87,13 @@ const login = (formEl: FormInstance | undefined) => {
       //   type: "success",
       //   duration: 3000
       // });
-      ElNotification({
-        title: "React 付费版本 🔥🔥🔥",
-        dangerouslyUseHTMLString: true,
-        message: "预览地址：<a href='https://pro.spicyboy.cn'>https://pro.spicyboy.cn</a>",
-        type: "success",
-        duration: 8000
-      });
+      // ElNotification({
+      //   title: "React 付费版本 🔥🔥🔥",
+      //   dangerouslyUseHTMLString: true,
+      //   message: "预览地址：<a href='https://pro.spicyboy.cn'>https://pro.spicyboy.cn</a>",
+      //   type: "success",
+      //   duration: 8000
+      // });
     } finally {
       loading.value = false;
     }
@@ -122,5 +122,5 @@ onBeforeUnmount(() => {
 </script>
 
 <style scoped lang="scss">
-@import "../index.scss";
+@import "../index";
 </style>
