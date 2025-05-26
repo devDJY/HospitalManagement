5bb681008adfef1399f8481cb78fc6cd01b1d247
ge <528636582@qq.com>
2025-05-26 10:16:52 +0800

    更新

--- src/api/modules/fileInfo.ts
+++ src/api/modules/fileInfo.ts
@@ -1,22 +1,29 @@
 import http from "@/api";
+import { useUserStore } from "@/stores/modules/user";
+const userStore = useUserStore();
 // 文件-形式审查-分页查询
 export const fileInfoList = params => {
-  return http.get("/fileInfo/list", params, { loading: false });
+  let pa = { userId: userStore.userInfo.id, ...params };
+  return http.get("/fileInfo/list", pa, { loading: false });
 };
 ///fileController/cert/list
 export const fileControllerCertList = params => {
-  return http.get("/fileController/cert/list", params, { loading: false });
+  let pa = { userId: userStore.userInfo.id, ...params };
+  return http.get("/fileController/cert/list", pa, { loading: false });
 };
 //件-回收-查询列表
 export const fileControllerRecycleList = params => {
-  return http.get("/fileController/recycle/list", params, { loading: false });
+  let pa = { userId: userStore.userInfo.id, ...params };
+  return http.get("/fileController/recycle/list", pa, { loading: false });
 };
 //文件-销毁-查询列表
 export const fileControllerDestroyList = params => {
-  return http.get("/fileController/destroy/list", params, { loading: false });
+  let pa = { userId: userStore.userInfo.id, ...params };
+  return http.get("/fileController/destroy/list", pa, { loading: false });
 };
 //
 //文件-遗失-查询列表
 export const fileControllerLoseList = params => {
-  return http.get("/fileController/lose/list", params, { loading: false });
+  let pa = { userId: userStore.userInfo.id, ...params };
+  return http.get("/fileController/lose/list", pa, { loading: false });
 };
